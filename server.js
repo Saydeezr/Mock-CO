d1cae629a39915c72b7a9f44e91ec2a6e2feee5d
Saydeezr <saydeezr@gmail.com>
2024-05-21 11:03:38 -0400

    started the inquirer for questioning and set up express

---
+++ server.js
@@ -0,0 +1,20 @@
+const inquirer = require('inquirer');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const initialEntry = inquirer.prompt([
+        { type: 'list',
+          message: 'What would you like to do?',
+          choices: ['View All Departments', 'View All Roles','View All Employees', 'Add Department', 
+                    'Add A Role', 'Add An Employee', 'Update An Employee Role'],
+          name: 'action'
+        }
+    ]) 
+    .then(initialEntry => {
+
+    })
