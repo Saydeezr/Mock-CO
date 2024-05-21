b203bb494bebc495c30cbea08389aa2f36b8366f
Saydeezr <saydeezr@gmail.com>
2024-05-21 13:38:45 -0400

    took out express(accident)

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
 const inquirer = require('inquirer');
-const express = require('express');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 const initialEntry = inquirer.prompt([
         { type: 'list',
@@ -14,11 +8,11 @@ const initialEntry = inquirer.prompt([
                     'Add A Role', 'Add An Employee', 'Update An Employee Role'],
           name: 'initial request'
         }
-    ]) //add if stament?
+    ]) 
     .then(initialEntry => {
-
+        switch(initialEntry.options){
+            case 'View All Departments':
+                viewDepartments();
+                break
+        }
     })
-
-app.listen(PORT, () => { 
-    console.log('listening on port')
-});
